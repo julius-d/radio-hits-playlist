794bca13b8a061a95d3eea929a6721f3be6fd3de
Julius <30121440+julius-d@users.noreply.github.com>
2024-02-03 18:03:38 +0100

    Reorder a playlist

---
+++ src/main/java/com/github/juliusd/PlaylistShuffel.java
@@ -0,0 +1,49 @@
+package com.github.juliusd;
+
+import com.neovisionaries.i18n.CountryCode;
+import org.apache.hc.core5.http.ParseException;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Playlist;
+import se.michaelthelin.spotify.model_objects.specification.PlaylistTrack;
+import se.michaelthelin.spotify.requests.data.playlists.GetPlaylistRequest;
+import se.michaelthelin.spotify.requests.data.playlists.ReorderPlaylistsItemsRequest;
+
+import java.io.IOException;
+
+public class PlaylistShuffel {
+
+    public static void main(String[] args) throws IOException, ParseException, SpotifyWebApiException {
+        System.out.println("Start");
+
+        SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                .setAccessToken( System.getProperty("spotifyAccessToken"))
+                .setRefreshToken(System.getProperty("spotifyRefreshToken"))
+                .build();
+
+        String playlistId = "***REMOVED***";
+        var playlist = spotifyApi.getPlaylist(playlistId)
+                .market(CountryCode.DE)
+                .build().execute();
+
+        printPlayList(playlist);
+        Integer totalAmountOfTracks = playlist.getTracks().getTotal();
+        moveFirst5TracksToTheEndOfThePlaylist(spotifyApi, playlistId, totalAmountOfTracks);
+
+
+        System.out.println("Done");
+    }
+
+    private static void printPlayList(Playlist playlist) {
+        System.out.println("Playlist: "+ playlist.getName());
+        for (PlaylistTrack track : playlist.getTracks().getItems()) {
+            System.out.println(track.getTrack().getName()+" "+track.getTrack().getId());
+        }
+    }
+
+    private static void moveFirst5TracksToTheEndOfThePlaylist(SpotifyApi spotifyApi, String playlistId, Integer totalAmountOfTracks) throws IOException, SpotifyWebApiException, ParseException {
+        var reorderPlaylistsItemsRequest = spotifyApi.reorderPlaylistsItems(playlistId, 0, totalAmountOfTracks).range_length(5).build();
+        reorderPlaylistsItemsRequest.execute();
+    }
+
+}
