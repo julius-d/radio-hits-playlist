0bc8ef64c65ef07616c9f9bfbc080186d551c6f2
Julius <30121440+julius-d@users.noreply.github.com>
2024-02-08 18:57:56 +0100

    Clean up

--- src/main/java/com/github/juliusd/radiohitsplaylist/source/berlinhitradio/BerlinHitRadioClient.java
+++ src/main/java/com/github/juliusd/radiohitsplaylist/source/berlinhitradio/BerlinHitRadioClient.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 interface BerlinHitRadioClient {
 
-
   @Headers({
     "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0",
     "Accept: application/json, text/plain, */*",
